aff305f74132a01b56a4268b339a9051f0fbd604
yooonar <aryooon33@gmail.com>
2020-12-31 01:38:42 +0900

    select, result 페이지 생성 - django.shortcuts 을 이용한 문법 생략, html 에서의 django 문법

--- django/first-django/first/views.py
+++ django/first-django/first/views.py
@@ -1,5 +1,9 @@
 # 각 프로젝트 별 설정 - startapp 명령어로 생성된 파일(명령어: `python3 manage.py startapp first`)
 # 백엔드 코드를 정의하고 구현하는 파일(데이터 처리 구현)
+"""django.shortcuts 를 import 하면?
+
+각 메소드별로 template = loader.get_template('index.html') 를 생략할 수 있다.
+"""
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -14,7 +18,8 @@ from datetime import datetime
 # view 파일에서는 기본적으로 index 를 정의하면 request(object) 를 받을 수 있다.
 def index(request):
     # loader.get_template('파일명') <- 템플릿 호출
-    template = loader.get_template('index.html')
+    # django.shortcuts 으로 선언 생략 가능
+    # template = loader.get_template('index.html')
 
     # 현재 시간 출력
     now = datetime.now()
@@ -25,14 +30,24 @@ def index(request):
     }
 
     # template.render(context, request) <- 템플릿 렌더링
-    return HttpResponse(template.render(context, request))
+    # django.shortcuts 으로 선언 생략 가능
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
 
 
 def select(request):
     message = "수 하나를 입력해주세요."
-    return HttpResponse(message)
+    context = {
+        'number': 3,
+    }
+
+    # return HttpResponse(message)
+    return render(request, 'select.html', context)
 
 
 def result(request):
     message = "추첨 결과입니다."
-    return HttpResponse(message)
+    context = {}
+
+    # return HttpResponse(message)
+    return render(request, 'result.html', context)
