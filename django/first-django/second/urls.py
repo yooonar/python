a278b59e6d7aba53eed4b3a7b3ec1725638cfd4f
yooonar <aryooon33@gmail.com>
2021-01-01 20:36:34 +0900

    Django 기본 폼 생성(url path 연결) - second 웹 앱에 유효성 체크

--- django/first-django/second/urls.py
+++ django/first-django/second/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('list/', views.list, name="list"),
     path('create/', views.create, name="create"),
+    path('confirm/', views.confirm, name="confirm"),
 ]
