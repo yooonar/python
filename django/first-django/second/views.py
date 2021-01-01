85caa3bb1f3760542c3dcba7e8c13e4fafdda4fe
yooonar <aryooon33@gmail.com>
2021-01-01 20:35:54 +0900

    Django 기본 폼 생성(뷰 페이지) - second 웹 앱에 유효성 체크

--- django/first-django/second/views.py
+++ django/first-django/second/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 from second.models import Post
 
+# 리다이렉트 import
+from django.http import HttpResponseRedirect
+
 # django 기본 폼 가져오기
 from .forms import PostForm
 
@@ -17,3 +20,15 @@ def list(request) :
 def create(request) :
     form = PostForm()
     return render(request, 'second/create.html', {'form': form})
+
+
+def confirm(request) :
+    # object 생성자를 넘기면 자동으로 맵핌됨
+    form = PostForm(request.POST)
+
+    # 폼 유효성 체크
+    if form.is_valid() :
+        return render(request, 'second/confirm.html', {'form':form})
+
+    # 유효성 체크에 실패하면 다시 입력창으로 이동
+    return HttpResponseRedirect('/create/')
