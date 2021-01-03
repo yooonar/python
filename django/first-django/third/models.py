b55fdda54c1e33e604e368ba22a8f0ddf0434066
yooonar <aryooon33@gmail.com>
2021-01-03 21:54:30 +0900

    데이터 조회(모델 추가) - matemigrations -> migrate

--- django/first-django/third/models.py
+++ django/first-django/third/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+# 모델 구현
+class Restaurant(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=200)
+
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
