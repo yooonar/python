94b70796699c27cfc2d8048c9bf9bb53c578c208
yooonar <aryooon33@gmail.com>
2021-01-04 21:50:20 +0900

    파이썬 쉘 노트(마이그레이션) - 1. 마이그레이션(초기화), 2. 마이그레이트(sql적용)

--- django/orm/pythonshell.py
+++ django/orm/pythonshell.py
@@ -30,3 +30,43 @@ class Restaurant(models.Model):
 ===========================================
 
 """
+
+
+"""파이썬 쉘을 사용하기 위한 준비 작업: 모델 마이그레이션
+
+1. 마이그레이션
+생성한 웹 앱(third) migrations 폴더에 0001_initial.py 모델 초기화 파일이 생김
+명령어: python manage.py makemigrations
+
+===========================================
+~/Documents/study/python/django/first-django main*
+venv ❯ python manage.py makemigrations
+Migrations for 'third':
+  third/migrations/0001_initial.py
+    - Create model Restaurant
+===========================================
+
+
+2. 마이그레이트
+1번에서 초기화한 파일(0001_initial.py)을 기준으로 db.sqllite3 에 입력됨
+명령어: python manage.py migrate
+
+===========================================
+~/Documents/study/python/django/first-django main* ⇡
+venv ❯ python manage.py migrate
+Operations to perform:
+  Apply all migrations: admin, auth, contenttypes, second, sessions, third
+Running migrations:
+  Applying third.0001_initial... OK
+===========================================
+
+
+3. 마이그레이션 적용 현황 보기
+명령어: python manage.py showmigrations 앱이름
+
+
+4. 지정한 마이그레이션의 SQL 내용 보기
+명령어: python manage.py sqlmigrate 앱이름 마이그레이션이름
+
+
+"""
