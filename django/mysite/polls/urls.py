cde0917cc8d8f49e6f89849e3f8df06a09844d3c
yooonar <aryooon33@gmail.com>
2022-08-17 04:10:20 +0900

    url patterns

--- django/mysite/polls/urls.py
+++ django/mysite/polls/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # 메인 페이지 /polls/
+    path('', views.index, name='index'),
+    # 상세 페이지 /polls/1/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # 결과 페이지 /polls/1/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # 투표 페이지 /polls/1/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
