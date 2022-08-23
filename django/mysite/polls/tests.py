af263307bb5c45867fa2cc3cfdeca4f353ee7bb1
yooonar <aryooon33@gmail.com>
2022-08-24 00:54:21 +0900

    indexView에 과거, 미래 데이터 입력 후 테스트

--- django/mysite/polls/tests.py
+++ django/mysite/polls/tests.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 from .models import Question
 
 
@@ -58,3 +59,66 @@ class QuestionModelTests(TestCase):
         OK
         Destroying test database for alias 'default'...
         """
+
+
+# 테스트 데이터를 만들기 위한 함수
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+
+    # 데이터가 없는 경우
+    def test_no_questions(self):
+        response = self.client.get(reverse('polls:index'))
+        # assertEqual 같은 데이터인지 비교하는 경우
+        self.assertEqual(response.status_code, 200)
+        # assertContains 포함되어 있는지 확인하는 경우
+        self.assertContains(response, "No polls are available.")
+        # assertQuerysetEqual Queryset인 경우
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    # 데이터가 과거인 경우, 데이터가 나오지 않으면 문제가 있는 것!
+    def test_past_question(self):
+        question = create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question],  # 데이터가 있는지 확인
+        )
+
+    # 데이터가 미래인 경우, 데이터가 나오면 문제가 있는 것!
+    def test_future_question(self):
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])  # 데이터가 비어있는지 확인
+
+    # 데이터가 과거 1, 미래 1 인 경우, 과거 데이터만 나와야 정상! 미래 데이터가 나오면 문제 있는 것!
+    def test_future_question_and_past_question(self):
+        question1 = create_question(question_text="Past question.", days=-30)
+        question2 = create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question1]  # 과거 데이터만 나오는지 확인
+        )
+
+    # 데이터가 과거 2인 경우, 2개의 데이터가 나와야 정상!
+    def test_two_past_questions(self):
+        question1 = create_question(question_text="Past question 1.", days=-30)
+        question2 = create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question2, question1]  # 과거 데이터가 두개 나오는지 확인
+        )
+
+    """
+    indexView test 실행 결과
+    Ran 8 tests in 0.015s
+    
+    OK
+    Destroying test database for alias 'default'...
+    """
--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect  # , HttpResponse, Http404
 from .models import Question, Choice
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic  # generic view 사용
 from django.template import loader
 
@@ -12,7 +13,12 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """ Return the last five published questions. """
-        return Question.objects.order_by('-pub_date')[:5]
+        # 미래의 날짜가 나오지 않도록 filter를 넣어 조건을 입력
+        # 현재 시간보다 작거나 같은 데이터를 가져옴
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+        # return Question.objects.order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
