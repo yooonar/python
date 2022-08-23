4955b6f585b87297d44b5b5bf621a49ce4fa8a38
yooonar <aryooon33@gmail.com>
2022-08-23 22:30:13 +0900

    form을 이용해 데이터를 받아와서 처리하기

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect  # , Http404
+from .models import Question, Choice
+from django.urls import reverse
 from django.template import loader
 
 
@@ -48,9 +49,37 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = 'You\'re looking at the results of question %s.'
-    return HttpResponse(response % question_id)
+    # response = 'You\'re looking at the results of question %s.'
+    # return HttpResponse(response % question_id)
+
+    # 넘겨받은 question_id를 통해 question 데이터 조회
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse('You\'re voting on question %s.' % question_id)
+    # question_id 를 넘겨받아 데이터를 조회
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        # question에 대해 외래키를 갖는 선택지를 가져온다.
+        # request.POST['choice_id'] = 템플릿에서 넘겨받은 input 값의 이름
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+
+    # selected_choice 가 없는 경우 except 문을 탐
+    except(KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+          'question': question,
+          'error_message': "You didn't select a choice.",
+        })
+
+    # selected_choice 가 있는 경우 아래를 실행
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        # post로 호출된 경우는 HttpResponseRedirect 로 페이지 이동
+        # reverse 는 페이지 이동할 때 하드코딩 하지 않고 이동할 수 있도록 사용한 것
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+    # return HttpResponse('You\'re voting on question %s.' % question_id)
